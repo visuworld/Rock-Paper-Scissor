c8487f33aa64e88e9c4fcba01eb41929e43b42bd
Vishal Yadav <vishalyadav27100@gmail.com>
2025-06-13 19:12:58 +0000

    script for game

---
+++ script.js
@@ -0,0 +1,22 @@
+function playGame(playerChoice) {
+    const choices = ["rock","paper","scissor"];
+    const computer_choice = choices[Math.floor(Math.random()* choices.length)];
+
+    let result = "";
+    if (playerChoice === computer_choice){
+        result = "It's a tie!";
+    }
+    else if (
+        (playerChoice === "rock" && computer_choice === "scissor") ||
+        (playerChoice === "paper" && computer_choice === "rock") ||
+        (playerChoice === "cissor" && computer_choice === "paper")
+    ){
+        result = "You Win!";
+    }
+    else{
+        result = "Computer Wins!"
+    }
+
+    document.getElementById("result").innerText = `Computer choose ${computer_choice}. 
+    ${result}`;
+}
